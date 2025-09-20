2c91a69ece336d9b9ea63a417ee006ba781ddc27
ThatGuyyCoder <77430526+ThatGuyyCoder@users.noreply.github.com>
2025-09-20 14:06:15 +0300

    Adjust moon icon centering

--- src/templates.js
+++ src/templates.js
@@ -29,7 +29,7 @@ function layout({ title, body, user, flash }) {
       <path d="M12 3v2m0 14v2m9-9h-2M5 12H3m15.364-7.364-1.414 1.414M7.05 16.95l-1.414 1.414M18.364 18.364l-1.414-1.414M7.05 7.05 5.636 5.636"></path>
     </svg>
     <svg class="icon-moon" viewBox="0 0 24 24" aria-hidden="true" focusable="false" stroke="currentColor" stroke-width="1.8" fill="none" stroke-linecap="round" stroke-linejoin="round">
-      <path d="M17 12.79A9 9 0 0 1 7.21 3a7 7 0 1 0 9.79 9.79Z"></path>
+      <path d="M18.3 12.79A9 9 0 0 1 8.51 3a7 7 0 1 0 9.79 9.79Z"></path>
     </svg>
   </button>`;
 
